2737b42769d126d2c5237a06021e0f508a83723a
Jon <jonny@theorphan.net>
2015-04-24 22:38:59 -0700

    Fix baseAd scenario - wasn't initialized properly on refresh

--- appyads/src/main/java/com/appyads/services/AppyAdManager.java
+++ appyads/src/main/java/com/appyads/services/AppyAdManager.java
@@ -422,6 +422,7 @@ public class AppyAdManager extends ViewFlipper {
         if (baseAd > lastAd) baseAd = lastAd;
         if (baseViewIndex > lastAd) baseViewIndex = lastAd;
         if (baseViewIndex < 0) baseViewIndex = 0;
+        curAd = baseViewIndex;
     }
 
     public boolean AdsAreOn() {
